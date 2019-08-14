98ce06707b4eee235f7bee2a27edae2674080062
Shikhar <shikhar.raizada@nineleaps.com>
2019-08-14 17:23:13 +0530

    Project setup completed

--- Nominate/src/main/java/com/nominate/repository/SampleNominaterepository.java
+++
@@ -1,8 +0,0 @@
-package com.nominate.repository;
-
-import com.nominate.model.Samplenominate;
-import org.springframework.data.repository.CrudRepository;
-
-public interface SampleNominaterepository extends CrudRepository<Samplenominate, String> {
-
-}
